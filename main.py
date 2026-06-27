2a01185616d7a32ce68ceebbfba7f4bff2c25bc8
uday19pratap <uday19pratap@gmail.com>
2026-06-28 01:38:08 +0530

    B: rotating player

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 def main():
-    pygame.init()
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    pygame.init()
     screen: Surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt: float = 0.0
+    player: Player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    screen.fill("black")
     while True: 
         log_state()
         for event in pygame.event.get() :
             pass
             if(event.type == pygame.QUIT) :
                return
-            
-        screen.fill("black")
+               
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
