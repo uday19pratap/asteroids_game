44a806e7cd8da103a859b25cfbf7dfaf32423363
uday19pratap <uday19pratap@gmail.com>
2026-06-28 02:27:22 +0530

    D: update and draw with groups

--- main.py
+++ main.py
@@ -12,6 +12,11 @@ def main():
     screen: Surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt: float = 0.0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    
     player: Player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
     while True: 
@@ -21,8 +26,12 @@ def main():
             if(event.type == pygame.QUIT) :
                return
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        
+        updatable.update(dt)
+        for thing in drawable : 
+            thing.draw(screen)
+        #player.update(dt)
+        #player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
